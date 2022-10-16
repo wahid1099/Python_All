7976151a593959a536a79be297c3a7c7d712b5d8
wahid1099 <wahidahmed890@gmail.com>
2022-10-16 21:17:25 +0600

    adding tree

--- Basic/Dsa WIth python/Linkedlist.py
+++ Basic/Dsa WIth python/Linkedlist.py
@@ -7,6 +7,22 @@ class Node:
 class Linkedlist:
     def __init__(self):
         self.head=None
+    
+    def insert_at_positon(self,pos,val):
+        newNode=Node(val)
+        if pos==0:
+            newNode.next=self.head
+            self.head=newNode
+
+        else:    
+         temp=self.head
+         for i in range(pos-1):
+            temp=temp.next
+            if temp==None:
+                print("Out of bounds")
+                return
+         newNode.next=temp.next
+         temp.next=newNode
 
     def insert_at_tail(self,val):
         NewNode =Node(val)
@@ -25,7 +41,44 @@ class Linkedlist:
          while tmp!= None:
           print(tmp.val)
           tmp=tmp.next
+    
+    def delete_at_positon(self,pos):
+        if pos==0:
+          delnode=self.head
+          self.head=self.head.next
+          del delnode
 
+        else:
+            temp=self.head
+            for i in range(pos-1):
+                temp=temp.next
+                if temp==None:
+                    print("Out of bound")
+                    return
+            
+            delnode=temp.next
+            if temp.next==None:
+                
+                return
+            temp.next=temp.next.next
+            del delnode
+
+    def reverse(self):
+        # base case
+        if self.head.next==None:
+            return self.head
+        #head in save first save will have 10 then 20 then 30 and so on
+        save=self.head
+        # head next value in head
+        self.head=self.head.next
+        # self.reverse() is like we do in reverse(head->next)
+        newHead=self.reverse()
+        # save->n->n means that saveing value of its own to next pointer
+        #like 30 is save and 40 save.next.next so saving 30 in 40
+        save.next.next=save
+        save.next=None
+        return newHead
+   
 
 
 def main():
@@ -34,7 +87,12 @@ def main():
     list.insert_at_tail(20)
     list.insert_at_tail(30)
     list.insert_at_tail(40)
+    # list.insert_at_positon(6,100)
+    # list.delete_at_positon(3)
+    list.reverse()
     list.print_list()
+
+
                 
 
 
--- Opp_Intro/Day1/calculator.py
+++ Opp_Intro/Day1/calculator.py
@@ -1,14 +1,44 @@
-class Calculator:
-    def add(self, a,b):
-        return a+b
+# class Calculator:
+#     def add(self, a,b):
+#         return a+b
       
-    def subtract(self, a,b):
-        return a-b
-    def multiply(self, a,b):
-        return a*b
-    def divide(self, a,b):
-        return a/b
+#     def subtract(self, a,b):
+#         return a-b
+#     def multiply(self, a,b):
+#         return a*b
+#     def divide(self, a,b):
+#         return a/b
 
-my_calcultor=Calculator()
-add=my_calcultor.add(5,5)
-print(add)
+# my_calcultor=Calculator()
+# add=my_calcultor.add(5,5)
+# print(add)
+
+# class Sales:
+#     def __init__(self, id):
+#         self.id = id
+#         id = 100
+
+# val = Sales(123)
+# print (val.id)
+
+# class People():
+#     def __init__(self, name):
+#       self.name = name
+
+#     def namePrint(self):
+#       print(self.name)
+
+
+
+# person1 = People("Emma")
+# person2 = People("Watson")
+# person2.namePrint()
+
+class Person:
+    def __init__(self, id):
+        self.id = id
+
+sam = Person(100)
+sam.__dict__['age'] = 49
+
+print (sam.age + len(sam.__dict__))
